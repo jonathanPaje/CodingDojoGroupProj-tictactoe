0e3391694b0dbdde9cda581403e51fd1f2a5e6b4
JonathanIsiahPaje <jpaje1227@gmail.com>
2021-02-28 18:37:02 -0800

    added javascipt, html, and css for game

--- tictactoe/views.py
+++ tictactoe/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from . import gameplay
 def index(request):
     context={
-        "board" : gameplay.display_board()
+        "board" : gameplay.display_board(),
     }
     return render(request, "home.html", context)
+
+def index2(request):
+    return render(request, 'home2.html')
 
 # Create your views here.
